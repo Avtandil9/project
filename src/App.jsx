23bcd0ea6a9eb083ac5e10a672d59f0fe6f551e4
Avtandil <avtandilbektemirov794@gmail.com>
2025-04-10 19:59:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+function App() {
+  return (
+    <>
+      <body>
+        <div class="resume-container">
+          <header>
+            <h1>Автандил Бектемиров</h1>
+            <p class="title">Front-end разработчик</p>
+            <p class="contact">
+              Телефон: +996 222 415 637 | Email: bektemirovavtandil777@gmail.com
+            </p>
+          </header>
+
+          <section class="experience">
+            <h2>Опыт работы</h2>
+            <div class="job">
+              <p class="company">
+                Студент "Front-end разработчик" — Сентябрь 2024 - по текущее
+                время
+              </p>
+              <ul>
+                <li>
+                  Разработка веб-сайтов с использованием JavaScript, React,
+                  Node.js.
+                </li>
+                <li>Работа с JavaScript и фреймворками.</li>
+                <li>Отправка и получение данных с сервера через API.</li>
+              </ul>
+            </div>
+            <div class="job">
+              <h3>Работа в команде</h3>
+              <p class="company">Часто взаимодейтвие с</p>
+              <ul>
+                <li>Дизайнерами(по макетам в Figma).</li>
+                <li>Back-end разработчиками(по API).</li>
+                <li>Менеджерам (по требованиям).</li>
+              </ul>
+            </div>
+          </section>
+
+          <section class="education">
+            <h2>Образование</h2>
+            <div class="degree">
+              <h3>СОШ №94</h3>
+              <p class="school">Сентябрь 2015 - Май 2024</p>
+              <h3>ПЛИТ №99 при Президенте КР</h3>
+              <p class="institution">Сентябрь 2024 - по текущее время</p>
+            </div>
+          </section>
+
+          <section class="skills">
+            <h2>Навыки</h2>
+            <ul>
+              <li>HTML, CSS, JavaScript, React.</li>
+            </ul>
+          </section>
+
+          <footer>
+            <p>Спасибо за внимание!</p>
+          </footer>
+        </div>
+      </body>
+    </>
+  );
+}
+
+export default App;
